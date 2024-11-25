45a59bb9bb20e6ff8970836b9a2aa30d254b2cfe
Toni <toni.cantarutti@gmail.com>
2024-11-25 12:38:30 +0100

    Add: Voting steps

--- front/src/components/features/Contract/ContractActions.jsx
+++ front/src/components/features/Contract/ContractActions.jsx
@@ -6,11 +6,12 @@ import { useRef } from "react";
 import { useContractWriter } from "@/hooks/useContractWriter";
 import { toast } from "sonner"
 import { useEffect } from "react";
+import { useContractEventsStore } from "@/hooks/useContractEvents";
 
-
-const ContractActions = ({ address, isOwner, isVoter, currentState }) => {
+const ContractActions = ({ isOwner, isVoter, currentState, refetchAll }) => {
 
    const inputRef = useRef();
+   const { refreshEvents } = useContractEventsStore();
    const {
       // Write state
       hash,
@@ -21,40 +22,51 @@ const ContractActions = ({ address, isOwner, isVoter, currentState }) => {
       txError,
       // Actions
       caller,
+      data,
    } = useContractWriter();
 
 
    useEffect(() => {
-      toast('Hello World', {
-         position: 'top-center',
-      });
-   }, []);
+      if (txIsLoading)
+         toast("Waiting for confirmation...", {
+            position: 'top-center',
+         });
+   }, [txIsLoading]);
+
+   useEffect(() => {
+      if (hash)
+         toast(`Transaction confirmed: ${hash}`, {
+            position: 'top-center',
+         });
+   }, [hash]);
 
    useEffect(() => {
       if (txIsSuccess) {
-         toast({
-            title: "Félicitations",
-            description: "Votre nombre a été inscrit dans la Blockchain",
-            className: "bg-lime-200",
+         toast("Transaction successful.", {
+            position: 'top-center',
          });
-         refetchEverything();
+         refetchAll();
+         refreshEvents();
+         if (inputRef.current) {
+            inputRef.current.value = "";
+         }
       }
-      if (txError) {
-         toast({
-            title: errorConfirmation.message,
-            status: "error",
-            duration: 3000,
-            isClosable: true,
+   }, [txIsSuccess]);
+
+   useEffect(() => {
+      if (txError)
+         toast(`${txError.shortMessage || txError.message}`, {
+            position: 'top-center',
          });
-      }
-      if (hash) {
-         toast({
-            title: "Transaction confirmed",
-            status: "error",
-            duration: 3000
+   }, [txError]);
+
+   useEffect(() => {
+      if (error)
+         toast(`${error.shortMessage || error.message}`, {
+            position: 'top-center',
          });
-      }
-   }, [txIsSuccess, txError, hash]);
+   }, [error]);
+
 
    const noRights = <p className="italic">You have no rights to perform any action</p>;
    if (!isOwner && !isVoter) {
@@ -78,14 +90,18 @@ const ContractActions = ({ address, isOwner, isVoter, currentState }) => {
                            onClick={async () => {
                               const address = inputRef.current.value;
                               await caller("addVoter", [address]);
-                              //inputRef.current.value = ""
                            }}
                         >
                            Add a voter
                         </Button>
                      </div>
                      <div className="flex flex-row gap-2">
-                        <Button className="w-[608px]">
+                        <Button
+                           onClick={async () => {
+                              await caller("startProposalsRegistering");
+                           }}
+                           className="w-[608px]"
+                        >
                            &gt;&gt; Start Proposals Registration
                         </Button>
                      </div>
@@ -94,9 +110,141 @@ const ContractActions = ({ address, isOwner, isVoter, currentState }) => {
             return noRights;
          }
       case 1:
-         return <p className="italic">You have no rights to perform any action</p>;
+         {
+            if (!isVoter && !isOwner)
+               return noRights;
+
+            const elements = [];
+
+            if (isVoter) {
+               elements.push(
+                  <div key="proposal" className="flex flex-row gap-2">
+                     <Input
+                        placeholder="Proposal"
+                        className="w-[400px]"
+                        ref={inputRef}
+                     />
+                     <Button
+                        className="w-[200px]"
+                        onClick={async () => {
+                           const proposal = inputRef.current.value;
+                           await caller("addProposal", [proposal]);
+                        }}
+                     >
+                        Add a proposal
+                     </Button>
+                  </div>
+               );
+            }
+
+            if (isOwner) {
+               elements.push(
+                  <div key="end-register" className="flex flex-row gap-2">
+                     <Button
+                        onClick={async () => {
+                           await caller("endProposalsRegistering");
+                           refetchRights();
+                        }}
+                        className="w-[608px]"
+                     >
+                        &gt;&gt; End Proposals Registering
+                     </Button>
+                  </div>
+               );
+            }
+
+            return <div className="flex flex-col gap-2">{elements}</div>;
+         }
       case 2:
-         return <p className="italic">You have no rights to perform any action</p>;
+         {
+            if (isOwner) {
+               return (
+                  <div key="end-register" className="flex flex-row gap-2">
+                     <Button
+                        onClick={async () => {
+                           await caller("startVotingSession");
+                           refetchRights();
+                        }}
+                        className="w-[608px]"
+                     >
+                        &gt;&gt; Start Voting Session
+                     </Button>
+                  </div>
+               );
+            }
+            else
+               return noRights;
+         }
+      case 3:
+         {
+            if (!isVoter && !isOwner)
+               return noRights;
+
+            const elements = [];
+
+            if (isVoter) {
+               elements.push(
+                  <div key="proposal" className="flex flex-row gap-2">
+                     <Input
+                        placeholder="Proposal ID"
+                        className="w-[400px]"
+                        ref={inputRef}
+                     />
+                     <Button
+                        className="w-[200px]"
+                        onClick={async () => {
+                           const proposal = inputRef.current.value;
+                           await caller("setVote", [proposal]);
+                        }}
+                     >
+                        Vote for proposal
+                     </Button>
+                  </div>
+               );
+            }
+
+            if (isOwner) {
+               elements.push(
+                  <div key="end-register" className="flex flex-row gap-2">
+                     <Button
+                        onClick={async () => {
+                           await caller("endVotingSession");
+                           refetchRights();
+                        }}
+                        className="w-[608px]"
+                     >
+                        &gt;&gt; End Voting Session
+                     </Button>
+                  </div>
+               );
+            }
+
+            return <div className="flex flex-col gap-2">{elements}</div>;
+         }
+      case 4:
+         {
+            if (isOwner) {
+               return (
+                  <div key="end-register" className="flex flex-row gap-2">
+                     <Button
+                        onClick={async () => {
+                           await caller("tallyVotes");
+                           refetchRights();
+                        }}
+                        className="w-[608px]"
+                     >
+                        &gt;&gt; Tally Votes
+                     </Button>
+                  </div>
+               );
+            }
+            else
+               return noRights;
+         }
+      case 5:
+         {
+            return noRights;
+         }
    }
 }
 
--- front/src/components/layout/Header.jsx
+++ front/src/components/layout/Header.jsx
@@ -2,8 +2,8 @@ import { ConnectButton } from "@rainbow-me/rainbowkit";
 
 const Header = () => {
    return (
-      <header className="flex p-4">
-         <p className="text-4xl font-bold pl-4 pt-2 ">Alyra Project 3</p>
+      <header className="flex pt-8 pb-4 pl-8 pr-8">
+         <p className="text-4xl font-bold pt-2 ">Alyra Project 3</p>
          <div className="flex-1"> </div>
          <ConnectButton />
       </header>
